cbd428a2fe9c8bbd5a2ff3509290d7d656068141
Bilal Punjabi <bilalthepunjabi@gmail.com>
2023-04-10 08:00:43 +0000

    --just quick reference

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,28 @@
 <template>
     <article class="main">
-        <h2 style="color:black">
-            USA SVG MAP
-        </h2>
-        <p style="color: black;">
-            {{ pointedLocation }}
-        </p>
-        <div class="map-container">
-            <Map :map="USA" :location-class="getLocationClass" :location-color="getLocationColor"
-                @mouseout="unpointLocation" @mousemove="moveOnLocation" />
+        <h1>Sample Page</h1>
+        <div class="some-specific-part-for-map">
+            <div id="map"></div>
+            <div>
+                <button v-if="!mapRed" @click="makeRed">Make Red</button>
+            </div>
         </div>
     </article>
 </template>
 
-<script>
-import MAP_SVGS from 'svg-maps';
-import Map from './components/Map.vue'
-export default {
-    name: 'App',
-    components: {
-        Map,
-    },
-    data() {
-        return {
-            USA: MAP_SVGS.USA,
-            pointedLocation: null,
-        }
-    },
-    methods: {
-        getLocationName(node) {
-            return node && node.attributes.name.value
-        },
-        pointLocation(event) {
-            this.pointedLocation = this.getLocationName(event.target)
-        },
-        unpointLocation(event) {
-            this.pointedLocation = null
-        },
-        getLocationClass(location, index) {
-            // Generate heat map
-            return `svg-map__location svg-map__location--heat${index % 4}`
-        },
-        getLocationColor(location, index) {
-            return 'blue';
-        }
-    },
-}
+<script setup>
+import { ref, computed, onMounted } from 'vue';
+
+const mapRed = ref(false);
+const simplemapsUsmap = computed(() => window.simplemaps_usmap);
+
+const makeRed = () => {
+    mapRed.value = true;
+    simplemapsUsmap.value.mapdata.main_settings.state_color = 'red';
+    simplemapsUsmap.value.refresh();
+};
+
+onMounted(() => {
+    simplemapsUsmap.value.load();
+});
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,38 @@
-import { createApp } from 'vue'
+import { createApp, ref, computed, onMounted } from 'vue';
 import App from './App.vue'
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.component('map-component', {
+    setup() {
+      const mapRed = ref(false);
+      const simplemapsUsmap = computed(() => window.simplemaps_usmap);
+  
+      const makeRed = () => {
+        mapRed.value = true;
+        simplemapsUsmap.value.mapdata.main_settings.state_color = 'red';
+        simplemapsUsmap.value.refresh();
+      };
+  
+      onMounted(() => {
+        simplemapsUsmap.value.load();
+      });
+  
+      return {
+        mapRed,
+        makeRed,
+        simplemapsUsmap
+      };
+    },
+  
+    template: `
+      <div id="map"></div>
+      <div>
+        <button v-if="!mapRed" @click="makeRed">Make Red</button>
+      </div>
+    `
+  });
+
+app.mount('#app')
